2f1507db20443481f65f5756b36630787c37021b
alejo9998 <ra.ariasr@uniandes.edu.co>
2018-12-09 20:31:47 -0500

    problema c

--- proyecto/src/problemaA.java
+++ proyecto/src/problemaA.java
@@ -1,9 +1,10 @@
 import java.util.Scanner;
 
-public class punto1 
+public class problemaA 
 {
 	public int metodo(int longitud,String arreglo,int posicion)
 	{
+		//declaración de las variables
 		String[] arr= arreglo.split(" ");
 		
 		int anterior = -1;
@@ -14,9 +15,10 @@ public class punto1
 		boolean sale=false;
 		boolean casiOrde=false;
 		
+		//validación de la longitud ingresada con la longitud del arreglo
 		if(arr.length-longitud!=0)
 		{
-			System.out.println("La longitud del arreglo debe ser igual a la longitud del arreglo ingresado");
+			System.out.println("La longitud del arreglo debe ser igual a la longitud ingresada");
 		}
 		else
 		{
@@ -34,40 +36,42 @@ public class punto1
 				{
 					longitudMomentanea++;
 					casiOrde=true;
+					//recursión del metodo 
 					longitudAux=metodo(longitud, arreglo, i+1);
 				}
 				else
+					
 				{
+					
+					
 					sale=true;
-					if(longitudMomentanea>longitudMaxima)
-					{
-						if(longitudMomentanea>longitudAux)
-						{
-							longitudMaxima=longitudMomentanea;
-						}
-						else
-						{
-							longitudMaxima=longitudAux;
-						}
-					}
-					else if(longitudAux>longitudMaxima)
-					{
-						longitudMaxima=longitudAux;
-					}
+					
 				}
 			}
 		}
+		
+		//validacion del mayor resultado posible
+	
+			if(longitudMomentanea>longitudAux)
+			{
+				longitudMaxima=longitudMomentanea;
+			}
+			else
+			{
+				longitudMaxima=longitudAux;
+			}
+		
 		
 		return longitudMaxima;
 	}
 
 	public static void main(String[] args) {
 		Scanner sc= new Scanner(System.in);
-		punto1 punto = new punto1();
+		problemaA punto = new problemaA();
 
 		while(true)
 		{
-			System.out.println("Ingrese longitud del arreglo");
+			
 
 			String longitud = sc.nextLine();
 			if(Integer.parseInt(longitud)==0)
@@ -77,7 +81,6 @@ public class punto1
 				break;
 			}
 
-			System.out.println("Ingrese el arreglo");
 
 			String arreglo= sc.nextLine();
 			
---
+++ proyecto/src/problemaC.java
@@ -0,0 +1,79 @@
+import java.util.Scanner;
+
+public class problemaC {
+
+
+	public void metodo(int longitud,String Arreglo)
+	{
+		String[] arr= Arreglo.split(" ");
+		int limiteMenor=Integer.parseInt(arr[0]);
+		int limiteSuperior=Integer.parseInt(arr[1]);
+		int resultado=0;
+		if(2*longitud-arr.length!=0)
+		{
+			System.out.println("La longitud ingresada no concuerda con la cantidad ingresada de datos");
+
+		}
+		else
+		{
+			int limInferiroMome=0;
+			int limSupMoMe=0;
+			for(int i =2;i<longitud*2;i+=2)
+			{
+				limInferiroMome=Integer.parseInt(arr[i]);
+				limSupMoMe=Integer.parseInt(arr[i+1]);
+
+				if(limiteSuperior<limInferiroMome)
+				{
+
+				}
+				else
+				{
+					
+					
+					if(limiteMenor<limInferiroMome)
+					{
+						limiteMenor=limInferiroMome;
+
+					}
+
+					if(limiteSuperior>limSupMoMe)
+					{
+
+						limiteSuperior=limSupMoMe;
+					}
+					
+				}
+			}
+			
+			resultado =(limiteSuperior+limiteMenor)/2;
+
+
+		}
+		System.out.println(resultado);
+	}
+
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		problemaC punto = new problemaC();
+
+		while(true)
+		{
+
+
+			String longitud = sc.nextLine();
+			if(Integer.parseInt(longitud)==0)
+			{
+				System.out.println("Programa terminado");
+				sc.close();
+				break;
+			}
+
+
+			String arreglo= sc.nextLine();
+
+			punto.metodo(Integer.parseInt(longitud), arreglo);
+
+		}
+	}
+}
